05b32261ef06f268c5e0d6420a5b123e92457d66
Michael <michaelh7301@gmail.com>
2025-12-26 11:06:12 -0800

    waypoints + managers

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static GameManager Instance {get; set;}
+    
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Instance = this;
+        }
+
+    }
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
